b5aeece00a310351ca7de10158eeaeaa7e8c7191
nasser <nasrimht24@gmail.com>
2017-04-04 12:50:43 +0200

    add test file

--- test_date.php
+++ test_date.php
@@ -1,5 +1,25 @@
 <?php
+class Base {
 
+	
+
+	public function connexion() {
+		try
+		{
+			$bdd = new PDO('mysql:host=localhost;dbname=gestionclub;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+		}
+		catch(Exception $e)
+		{
+		        die('Erreur : '.$e->getMessage());
+		}
+
+		return $bdd;
+	}
+}
+
+
+/*
 //la date d’aujourd’hui au format Jour-Mois-Année
 $dateDuJour = date('d-m-Y');
 
@@ -19,4 +39,37 @@ var_dump($i);
 $date->add($i);
 
 
-var_dump( $date->format('Y-m-d H:m:s'));
+var_dump( $date->format('Y-m-d H:m:s'));*/
+
+//recupere la liste de tout les formation.
+    function listeFormation()
+    {
+    	$bdd = new Base();
+    	$bdd = $bdd->connexion();
+
+    	$req = $bdd->query('SELECT moment From evenements');
+
+        $donnees = $req->fetchAll(PDO::FETCH_ASSOC);
+    	
+        return $donnees;
+    }
+
+var_dump( listeFormation()); die();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
